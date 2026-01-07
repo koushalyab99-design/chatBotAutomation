942de3e93cf2822f74f18924cac84de559db5f68
Koushalya <koushalyab99@gmail.com>
2026-01-07 22:43:49 +0530

    chatbot commit

---
+++ src/main/java/Koushalya/ChatBotAutomation/App.java
@@ -0,0 +1,7 @@
+package Koushalya.ChatBotAutomation;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/chatBotTest/StandaloneTest.java
@@ -0,0 +1,44 @@
+package chatBotTest;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class StandaloneTest {
+
+    public static void main(String[] args) {
+
+    	ChromeOptions options = new ChromeOptions();
+    	options.setAcceptInsecureCerts(true);
+    
+    	WebDriver driver = new ChromeDriver(options);
+
+    	driver.manage().window().maximize();
+    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    	driver.get("https://www.tvscredit.com/get-in-touch/");
+    	
+    	driver.findElement(By.id("ymDivBar")).click();
+    	
+    	driver.switchTo().frame("ymIframe");
+    	
+    	driver.findElement(By.xpath("//div[text()='New Customer']")).click();
+    	
+    	driver.findElement(By.xpath("//div[text()='Two-Wheeler Loans']")).click();
+    	
+    	driver.findElement(By.xpath("//div[text()='Eligibility Document']")).click();
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;
+    	
+    	js.executeScript("document.querySelector('#chatBoxMain').scrollBy(0,100)");
+    	
+
+
+    }
+}
